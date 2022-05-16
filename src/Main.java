82453a6c8a6b3258d3676f1655f02feba987fcb6
jivesh <jiveshm2000@gmail.com>
2022-05-17 00:08:19 +0800

    Add scanner that breaks string into tokens

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import lox.Lox;
+
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Booting up Lox v1.0!");
+        try {
+            Lox.main(args);
+        } catch (IOException e) {
+            System.out.println("Caught an IO exception: " + e.getMessage());
+        }
     }
 }
